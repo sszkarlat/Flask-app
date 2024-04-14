6dbbbd0ca4b8c333eac25236c16753f296361a53
sszkarlat <sszkarlat@student.agh.edu.pl>
2024-04-14 20:17:34 +0200

    Completed first flask app

--- greeting/app.py
+++ greeting/app.py
@@ -3,12 +3,20 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        name = request.form.get("name")
+        return render_template("greet.html", name=name)
     return render_template("index.html")
 
 
-@app.route("/greet")
-def greet():
-    name = request.args.get("name", "world")
-    return render_template("greet.html", name=name)
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
+
+
+# @app.route("/greet", methods=["POST"])
+# def greet():
+#     name = request.form.get("name", "world")
+#     return render_template("greet.html", name=name)
